4566916add92bd589d1694d54cb2a7602c4517a6
dbiswas <dipankar.biswas@oath.com>
2018-08-16 22:08:10 -0700

    Admin func added

--- contracts/ProofOfExistence.sol
+++ contracts/ProofOfExistence.sol
@@ -1,13 +1,22 @@
 pragma solidity ^0.4.18;
 
-contract ProofOfExistence{
+contract ProofOfExistence is Mortal{
 
+    mapping(address => bool) admins;
+    mapping( string => Document) documents;
+    mapping(address => UserUsageCount) usersUsage;
+    uint documentUploadPeriod = 120 seconds;
+    uint documentLimit = 3;
+    bool private stopped = false;
+
+    //Document idenfiers alongwith ipfs has of the document
     struct Document {
         bytes32 docHash;
         uint docTimestamp;
         string ipfsHash;
     }
 
+    //To keep track of all the documents owned by a user
     struct User {
         address addr;
         string userName;
@@ -17,9 +26,59 @@ contract ProofOfExistence{
 
     mapping (address => User) users;
 
-    event LogUploadDocument(address addr,string userName, bytes32 dochash, uint docTimestamp, string ipfsHash);
+    //Maintain user usage count to implement throtlling
+    struct UserUsageCount {
+        uint time;
+        uint count;
+    }
 
-    function uploadDocument(bytes32 _docHash,string _userName,string _ipfsHash) public returns(bool) {
+
+    //Events for logging
+    event LogUploadDocument(address _addr,string _userName, bytes32 _dochash, uint _docTimestamp, string _ipfsHash);
+    event LogAssignAdmin(address _sender, string _message);
+
+    //Modifiers
+    modifier onlyOwner(){require(msg.sender == owner, "msg sender is not owner"); _;}
+    modifier onlyAdmin(){require(admins[msg.sender] == true,"Sender is not Admin"); _;}
+    modifier stopInEmergency {if (!stopped) _;}
+    modifier onlyInEmergency {if (stopped) _;}
+
+    //Circuit Breaker to pause the contract in case of Emergency
+    function toggleContractActive() public 
+    onlyAdmin {
+        stopped = !stopped;
+    }
+
+    //Assign admin access
+    function assignAdminAccess(address _address) public
+    onlyOwner
+    returns(bool){
+        if(admins[_address] == false){
+            admins[_address] = true;
+            emit LogAssignAdmin(_address,"Admin Role assigned");
+            return true;
+        } else {
+            emit LogAssignAdmin(_address,"Either user does not exists or already has Admin access");
+        }
+        return false;
+    }
+
+    //Verify whether a user is already admin
+    function isAdmin(address _address) 
+    public 
+    view 
+    returns(bool){
+        if(admins[_address] == true){
+            return true;
+        }
+        return false;
+    }
+
+    //Upload document hash to Blockchain
+    function uploadDocument(bytes32 _docHash,string _userName,string _ipfsHash) 
+    public
+    stopInEmergency 
+    returns(bool) {
         //check whether the document already exists in the blockchain
         //if the document does not exist then add the document details and return true
         //if the document already exists then do nothing and return false
@@ -28,24 +87,37 @@ contract ProofOfExistence{
             users[msg.sender].userName = _userName;
             users[msg.sender].documentList.push(_docHash);
             users[msg.sender].documentDetails[_docHash] = Document(_docHash,block.timestamp,_ipfsHash);
-        
-            emit LogUploadDocument(msg.sender, _userName,_docHash, block.timestamp, _ipfsHash);
             
+            //Log document upload event
+            emit LogUploadDocument(msg.sender, _userName,_docHash, block.timestamp, _ipfsHash);
         }else {
             return false;
         }
         return true;
     }
 
+    /*
+    //verify whether user exceeded rate limit assigned
+    function verifyRateLimit()
+    public
+    returns(bool) {
+        //Input paramenters validation
+       //Document hash should not be empty and length should 256 bytes
+        UserUsageCount storage userUploadStats = usersUsage[msg.sender];
+
+    }
+    */
+
     function fetchDocument(bytes32 _docHash) public returns(bytes32, uint, string){
 
-        require( _docHash != 0 ,"Document Hash is mandatory");
+        require(_docHash != 0, "Document Hash is mandatory");
         
         Document storage document = users[msg.sender].documentDetails[_docHash];
         emit LogUploadDocument(msg.sender,users[msg.sender].userName,document.docHash, document.docTimestamp,document.ipfsHash);
         return(document.docHash,document.docTimestamp,document.ipfsHash);
     }
 
+    //To fetch all the documents for a user
     function fetchAllDocuments() public view returns(bytes32[]){
         return users[msg.sender].documentList;
     }
